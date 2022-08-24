7707ef2caebec15541b565d807c1b357d0905d3a
“Nicolas <nicolas.e.bron@gmail.com>
2022-08-24 17:25:17 -0400

    added operate function with switch statement

--- script.js
+++ script.js
@@ -14,6 +14,17 @@ const divide = function(a, b) {
     return a / b;
     };
 
-const operate = function() {
-
-};
+// Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+function operate(num1, num2, operator) {
+    switch (operator) {
+      case "+":
+        return add(num1, num2);
+      case "-":
+        return subtract(num1, num2);
+      case "*":
+        return multiply(num1, num2);
+      case "/":
+        return divide(num1, num2);
+    }
+  };
+  
